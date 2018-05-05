ef5685df0e1781795b85dda058e4538ecdfca462
Christopher Svanefalk <christopher.svanefalk@consultant.volvo.com>
2018-05-05 21:00:38 +0200

    Add user type selection.

--- src/Register/index.js
+++ src/Register/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {withStyles} from 'material-ui/styles';
 import Input, {InputLabel} from 'material-ui/Input';
-import {FormControl, FormHelperText} from 'material-ui/Form';
+import {FormControl, FormHelperText, FormControlLabel} from 'material-ui/Form';
+import Radio, {RadioGroup} from 'material-ui/Radio';
 import Button from 'material-ui/Button';
 import Grid from 'material-ui/Grid';
 
@@ -10,42 +11,74 @@ const styles = theme => ({
     root: {
         display: 'flex',
         flexWrap: 'wrap',
-        'text-align': 'center'
     },
     formControl: {
         margin: theme.spacing.unit,
         minWidth: 240,
-        'text-align': 'center'
     },
     selectEmpty: {
         marginTop: theme.spacing.unit * 2,
     },
+    centerGroup: {
+        'justify-content': 'center',
+        'flex-direction': 'row'
+    }
 });
 
-const RegisterView = ({classes}) => (
-    <form action="">
-        <Grid>
-            <FormControl className={classes.formControl}>
-                <Input id="name-input"/>
-                <FormHelperText>Alignment with an input</FormHelperText>
-            </FormControl>
-        </Grid>
-        <Grid>
-            <FormControl className={classes.formControl}>
-                <Input id="email-input"/>
-                <FormHelperText>Email (optional)</FormHelperText>
-            </FormControl>
-        </Grid>
-        <Grid>
-            <FormControl className={classes.formControl}>
-                <Input id="password-input" type={'password'}/>
-                <FormHelperText>Password</FormHelperText>
-            </FormControl>
-        </Grid>
-        <Grid>
-            <Button>Submit</Button>
-        </Grid>
-    </form>);
+class RegisterView extends Component {
+
+    state = {
+        userType: 'giver'
+    };
+
+    handleUserTypeChange = (event) => {
+        this.setState({userType: event.target.value});
+    }
+
+    render() {
+        const {classes} = this.props;
+
+        return (
+            <form action="">
+                <Grid>
+                    <FormControl className={classes.formControl}>
+                        <Input id="name-input"/>
+                        <FormHelperText>Alignment with an input</FormHelperText>
+                    </FormControl>
+                </Grid>
+                <Grid>
+                    <FormControl className={classes.formControl}>
+                        <Input id="email-input"/>
+                        <FormHelperText>Email (optional)</FormHelperText>
+                    </FormControl>
+                </Grid>
+                <Grid>
+                    <FormControl className={classes.formControl}>
+                        <Input id="password-input" type={'password'}/>
+                        <FormHelperText>Password</FormHelperText>
+                    </FormControl>
+                </Grid>
+                <RadioGroup
+                    aria-label="gender"
+                    name="gender2"
+                    className={classes.centerGroup}
+                    value={this.state.userType}
+                    onChange={this.handleUserTypeChange}
+                >
+                    <FormControlLabel value="giver"
+                                      control={<Radio color="primary"/>}
+                                      label="Giver"/>
+
+                    <FormControlLabel value="receiver"
+                                      control={<Radio color="primary"/>}
+                                      label="Reciever"/>
+                </RadioGroup>
+                <Grid>
+                    <Button>Submit</Button>
+                </Grid>
+            </form>);
+    }
+}
 
 RegisterView.propTypes = {
     classes: PropTypes.object.isRequired,
